7d39db4a961de98bc3cfefa18139ccfc3cdf2c46
Michał O <micavanco@gmai.com>
2018-12-31 20:38:33 +0100

    Recreated whole project in React from scratch - basic stuff

--- scripts/add-footer.js
+++
@@ -1,8 +0,0 @@
-/*   Script to add current year to footer paragraph  */
-(function () {
-    var today = new Date();
-    var year = today.getFullYear();
-
-    var footer = document.getElementById('footer_year');
-    footer.textContent = year.toString();
-}());
--- scripts/add-goodbyewindow.js
+++
@@ -1,9 +0,0 @@
-/* Script to show confirmation message before closing browser */
-(function (e) {
-    window.addEventListener('beforeunload', function (e) {
-        var confirmationMessage = 'Czy na pewno chcesz opuścić stronę?';
-
-        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
-        return confirmationMessage;                            //Webkit, Safari, Chrome
-    }, false);
-}());
