db59c632b27c18e7265a28722a6c996ab2eef8fd
Michał O <micavanco@gmai.com>
2019-01-17 16:13:09 +0100

    Added game field cover, game handlers and fixed bird's animation

--- src/components/HuntingGame.js
+++ src/components/HuntingGame.js
@@ -14,6 +14,16 @@ class HuntingGame extends Component{
             gameStatus: false
         };
 
+        document.addEventListener("keydown", (e)=>{
+            if(e.keyCode == 81 && this.state.gameStatus)
+            {
+                clearInterval(this.state.interval1);
+                this.state.gameStatus = false;
+                console.log("Game Over");
+                document.getElementById("menu-game").style.display = "flex";
+            }
+        });
+
         this.fly=this.fly.bind(this);
     }
 
@@ -22,6 +32,7 @@ class HuntingGame extends Component{
 
         if(!this.state.gameStatus)
         {
+            document.getElementById("menu-game").style.display = "none";
             this.state.gameStatus = true;
             bird1X+=450;
             document.getElementById("bird1").style.marginLeft = bird1X+"px";
@@ -40,8 +51,21 @@ class HuntingGame extends Component{
             margin: 120px 0 0 -50px;
         `;
 
+        const Menu = styled.div`
+            width: inherit;
+            height: inherit;
+            position: absolute;
+            background: rgba(254, 254, 254, 0.8);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 18px;
+            font-weight: 600;
+        `;
+
         return(
             <div className="img-box-left" onClick={this.onClickStartGame.bind(this)}>
+                <Menu id="menu-game">Kliknij aby rozpocząć grę</Menu>
                 <Game src="../../img/birds-land.png" id="birds-land" className="game">
                     <Bird src="../../img/bird1.png" id="bird1"/>
                 </Game>
@@ -53,6 +77,7 @@ class HuntingGame extends Component{
         if(bird1X === -50)
         {
             document.getElementById("bird1").style.display = "inline";
+            document.getElementById("bird1").style.marginTop = (Math.floor(Math.random() * 400)+110).toString()+"px";
             bird1X+=1;
         }
         else if(bird1X < 417 && bird1X > -50)
